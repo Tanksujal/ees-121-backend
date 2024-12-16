fd0f85e79edd5863023343e75318a4debb3eba2c
savee <sujaltank12@gmail.com>
2024-12-16 15:58:07 +0530

    savee

--- backend/routes/indexRoute.js
+++ backend/routes/indexRoute.js
@@ -2,4 +2,5 @@ const express = require('express')
 const routes = express.Router();
 routes.use('/auth',require('../routes/authRoute'))
 routes.use('/request',require('./requestRoute'))
+routes.use('/banner',require('./bannerRoutes'))
 module.exports = routes;
